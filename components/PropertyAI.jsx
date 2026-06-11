3564ced1733e76442160d0d18abf60dcd51d1204
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:35:26 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Trishla Bu
 
 Firm facts:
 - Firm: Trishla Builders
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Trishla Plus Homes, Peer Muchalla Road, Zirakpur, Punjab 140603
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
